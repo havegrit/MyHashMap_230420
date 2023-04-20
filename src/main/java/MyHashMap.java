085cb2984b8b82a50793b1d4fc8e8ae542c0b878
havegrit <shin_jaean@naver.com>
2023-04-20 09:48:23 +0900

    TDD GREEN: get() 메소드 구현

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -1,12 +1,26 @@
+import java.util.stream.IntStream;
+
 public class MyHashMap<T, T1> {
+    String[] keyData = new String[2];
+    Integer[] valueData = new Integer[2];
     Integer size = 0;
 
     public Integer put(String key, Integer value) {
-        size++;
+        keyData[size] = key;
+        valueData[size++] = value;
         return null;
     }
 
     public Integer size() {
         return size;
+    }
+
+    public Integer get(String key) {
+        for (int i = 0; i < size; i++) {
+            if (keyData[i].equals(key)) {
+                return valueData[i];
+            }
+        }
+        return null;
     }
 }
