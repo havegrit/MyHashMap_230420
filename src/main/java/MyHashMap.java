e7cecb1fd2a570ec7e1355b20836e6c202b83470
havegrit <shin_jaean@naver.com>
2023-04-20 09:33:49 +0900

    TDD GREEN: put() 메소드 실행 시, 사이즈 증가시키고 null 반환

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -1,3 +1,12 @@
-public class MyHashMap<String, Integer> {
+public class MyHashMap<T, T1> {
+    Integer size = 0;
 
+    public Integer put(String key, Integer value) {
+        size++;
+        return null;
+    }
+
+    public Integer size() {
+        return size;
+    }
 }
